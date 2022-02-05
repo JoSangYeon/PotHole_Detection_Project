f59a30e5ae22c8d2266efbf98b391fac0fa70e98
JoSangYeon <whtkddus98@kookmin.ac.kr>
2022-02-05 20:21:51 +0900

    main 뼈대 구현

--- main.py
+++ main.py
@@ -17,40 +17,38 @@ from inference import inference
 
 
 def main():
-    train_path = ""
-    test_path = ""
+    train_path = "Train_datasets_labels.csv"
+    test_path = "Validation_datasets_labels.csv"
 
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
 
     # your Data Pre-Processing
-    train_x, train_y = train_data.iloc[:, 1:], train_data.iloc[:, :1]
-    test_x, test_y = test_data.iloc[:, 1:], test_data.iloc[:, :1]
+    train_x, train_y = train_data.iloc[:, :1], train_data.iloc[:, 1:]
+    test_x, test_y = test_data.iloc[:, :1], test_data.iloc[:, 1:]
 
     # data split
     train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, stratify=train_y, random_state=17, test_size=0.1)
 
-    # Check Train, Valid, Test Image's Shape
-    print("The Shape of Train Images: ", train_x.shape)
-    print("The Shape of Valid Images: ", valid_x.shape)
-    print("The Shape of Test Images: ", test_x.shape)
+    train_data = pd.concat([train_x, train_y], axis=1)
+    valid_data = pd.concat([valid_x, valid_y], axis=1)
 
-    # Check Train, Valid Label's Shape
-    print("The Shape of Train Labels: ", train_y.shape)
-    print("The Shape of Valid Labels: ", valid_y.shape)
-    print("The Shape of Valid Labels: ", test_y.shape)
+    # Check Train, Valid, Test Data's Shape
+    print("The Shape of Train Data: ", train_data.shape)
+    print("The Shape of Valid Data: ", valid_data.shape)
+    print("The Shape of Test Data: ", test_data.shape, end="\n\n")
 
     # Create Dataset and DataLoader
-    train_dataset = MyDataset(train_x, train_y)
-    valid_dataset = MyDataset(valid_x, valid_y)
-    test_dataset = MyDataset(test_x, test_y)
+    train_dataset = MyDataset(train_data)
+    valid_dataset = MyDataset(valid_data)
+    test_dataset = MyDataset(test_data)
 
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=32)
+    valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=32)
 
     # label_tags
-    label_tags = ['T-Shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
+    label_tags = ["포트홀 없음", "포트홀", "보수 완료된 포트홀"]
 
     model = MyModel()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -58,13 +56,14 @@ def main():
     criterion = torch.nn.CrossEntropyLoss()
 
     # train
-    history = train(model, device, optimizer, criterion, 16, train_loader, valid_loader)
+    print("============================== Train ==============================")
+    history = train(model, device, optimizer, criterion, 20, train_loader, valid_loader)
 
     # Test
-    test_loss, test_acc = evaluate(model, device, test_loader, criterion)
+    print("============================== Test ==============================")
+    test_loss, test_acc = evaluate(model, device, criterion, test_loader)
 
-    # Inference
-    # inference(model, device, infer_dataloader)
+    torch.save(model, "models/model1.pt")
 
 
 if __name__ == '__main__':
