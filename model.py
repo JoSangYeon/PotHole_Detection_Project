197d6c7bc485555f2df6a5db314658a58a9d2d14
JoSangYeon <whtkddus98@kookmin.ac.kr>
2022-02-22 23:29:43 +0900

    Model 7 Upload

--- model.py
+++ model.py
@@ -515,6 +515,99 @@ class MyModel_6(nn.Module):
         x = self.fc2(x)  # (batch, 128) -> (batch, 3)
         return x
 
+class MyModel_7(nn.Module):
+    def __init__(self):
+        super(MyModel_7, self).__init__()
+
+        self.conv1_1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
+
+        self.conv2_1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+
+        self.conv3_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+
+        self.conv4_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+
+        self.conv5_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+
+        self.conv6_1 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1)
+
+        self.conv7_1 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1)
+        self.conv7_2 = nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.bn6 = nn.BatchNorm2d(512)
+        self.bn7 = nn.BatchNorm2d(1024)
+
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc2 = nn.Linear(128, 3)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.flatten = nn.AdaptiveAvgPool2d(1)
+        self.act_fn = nn.ReLU()
+
+        self.drop = nn.Dropout(p=0.25)
+
+    def forward(self, x):
+        x = self.conv1_1(x)         # (batch, 3, 320, 320) -> (batch, 16, 320, 320)
+        out = self.bn1(x)
+        out = self.act_fn(out)
+        out = self.conv1_2(out)       # (batch, 16, 320, 320) -> (batch, 16, 320, 320)
+        out += x
+        out = self.act_fn(out)
+        x = self.pool(out)            # (batch, 16, 320, 320) -> (batch, 16, 160, 160)
+
+        x = self.conv2_1(x)         # (batch, 16, 160, 160) -> (batch, 32, 160, 160)
+        x = self.bn2(x)
+        x = self.act_fn(x)
+        x = self.pool(x)            # (batch, 32, 160, 160) -> (batch, 32, 80, 80)
+
+        x = self.conv3_1(x)         # (batch, 32, 80, 80) -> (batch, 64, 80, 80)
+        x = self.bn3(x)
+        x = self.act_fn(x)
+        x = self.pool(x)            # (batch, 64, 80, 80) -> (batch, 64, 40, 40)
+
+        x = self.conv4_1(x)         # (batch, 64, 40, 40) -> (batch, 128, 40, 40)
+        out = self.bn4(x)
+        out = self.act_fn(out)
+        out = self.conv4_2(out)     # (batch, 128, 40, 40) -> (batch, 128, 40, 40)
+        out += x
+        out = self.act_fn(out)
+        x = self.pool(out)          # (batch, 128, 40, 40) -> (batch, 128, 20, 20)
+
+        x = self.conv5_1(x)         # (batch, 128, 20, 20) -> (batch, 256, 20, 20)
+        x = self.bn5(x)
+        x = self.act_fn(x)
+        x = self.pool(x)            # (batch, 256, 20, 20) -> (batch, 256, 10, 10)
+
+        x = self.conv6_1(x)         # (batch, 256, 10, 10) -> (batch, 512, 10, 10)
+        x = self.bn6(x)
+        x = self.act_fn(x)
+        x = self.pool(x)            # (batch, 512, 10, 10) -> (batch, 512, 5, 5)
+
+        x = self.conv7_1(x)         # (batch, 512, 5, 5) -> (batch, 1024, 5, 5)
+        out = self.bn7(x)
+        out = self.act_fn(out)
+        out = self.conv7_2(out)     # (batch, 1024, 5, 5) -> (batch, 1024, 5, 5)
+        out += x
+        out = self.act_fn(out)
+        x = self.pool(out)          # (batch, 1024, 5, 5) -> (batch, 1024, 2, 2)
+
+        x = self.flatten(x)         # (batch, 1024, 2, 2) -> (batch, 1024, 1, 1)
+        x = x.view(-1, 1024*1*1)    # (batch, 1024, 1, 1) -> (batch, 1024)
+
+        x = self.fc1(x)  # (batch, 1024) -> (batch, 128)
+        x = self.act_fn(x)
+        x = self.drop(x)
+
+        x = self.fc2(x)  # (batch, 128) -> (batch, 3)
+        return x
+
 def get_Model(class_name):
     try:
         Myclass = eval(class_name)()
@@ -523,7 +616,7 @@ def get_Model(class_name):
         print("Class [{}] is not defined".format(class_name))
 
 def main():
-    model = get_Model("MyModel_6").cuda()
+    model = get_Model("MyModel_7").cuda()
 
     summary_(model, input_size = (3, 320, 320), device = "cuda")
 
